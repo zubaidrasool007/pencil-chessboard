3b408daabb43c96200bf3a805a595a4cb0ab9c7b
Muhammad Zubaid Rasool <63468411+zubaidrasool007@users.noreply.github.com>
2022-01-10 01:59:00 +0500

    Added functionality to transfer FEN from one board to other

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { NgxChessBoardView } from 'ngx-chess-board';
+import { fromEvent } from "rxjs"
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css'],
 })
-export class BoardComponent implements OnInit {
-  constructor() {}
+export class BoardComponent implements OnInit, AfterViewInit {
+  @ViewChild('board', { static: false }) board!: NgxChessBoardView;
 
-  ngOnInit(): void {}
+  constructor() { }
+
+  ngOnInit(): void {
+    fromEvent(window, 'message').subscribe((event: any) => {
+      console.log("FEN", event.data)
+      this.board.setFEN(event.data)
+    })
+  }
+
+  ngAfterViewInit(): void {
+    const FEN = this.board.getFEN();
+    console.log({ FEN })
+  }
+
+  onMoveChange({fen}: any) {
+    window.postMessage(fen, `${window.location.href}iframe`)
+    console.log({ fen })
+  }
 }
